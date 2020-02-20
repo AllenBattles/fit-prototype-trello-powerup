276a39f98d108ebebcc2122a7760493ce6458a03
AllenBattles <abattles@frameworkit.net>
2020-02-20 17:36:20 -0600

    fx change

--- src/js/projectsummary.js
+++ src/js/projectsummary.js
@@ -5,7 +5,7 @@ t.render(function(){
   // make sure your rendering logic lives here, since we will
   // recall this method as the user adds and removes attachments
   // from your section
-  t.get('card')
+  t.getAll()
   .then(function(card){
       console.log(card);
     document.getElementById('project_summary_content').innerHTML  = "CARD ID = "  + card.idShort;
