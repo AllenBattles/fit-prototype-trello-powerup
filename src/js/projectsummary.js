54703db50a30ffdd89fc31bf3beabf7a3f4eec8b
AllenBattles <abattles@frameworkit.net>
2020-02-20 17:40:01 -0600

    script testing

--- src/js/projectsummary.js
+++ src/js/projectsummary.js
@@ -5,9 +5,15 @@ t.render(function(){
   // make sure your rendering logic lives here, since we will
   // recall this method as the user adds and removes attachments
   // from your section
-  t.getAll()
-  .then(function(card){
-      console.log(card);
-    document.getElementById('project_summary_content').innerHTML  = "CARD ID = "  + card.idShort;
+
+  t.get('card', 'shared')
+  .then(function (data) {
+    console.log(JSON.stringify(data, null, 2));
   });
+
+//   t.getAll()
+//   .then(function(card){
+//       console.log(card);
+//     document.getElementById('project_summary_content').innerHTML  = "CARD ID = "  + card.idShort;
+//   });
 });
