133565c64eca282583c4ece0caa2b629223bfb2e
AllenBattles <abattles@frameworkit.net>
2020-02-21 02:45:44 -0600

    json fix

--- src/js/projectsummary.js
+++ src/js/projectsummary.js
@@ -2,12 +2,6 @@
 var t = TrelloPowerUp.iframe();
 const API_BASE = 'https://glp2.csrsinc.com/api/dashboard/projectsummary';
 
-fetch(`${API_BASE}?id=217062.21.001`)
-.then(function(response) {
-  console.log(response); 
-}).catch(function(error) {  
-  console.log('Request failed', error)  
-});
 
 t.render(function(){
   // make sure your rendering logic lives here, since we will
@@ -23,7 +17,9 @@ t.render(function(){
 
         fetch(`${API_BASE}?id=217062.21.001`)
         .then(function(response) {
-          console.log(response); 
+            return response.json();
+        }).then(function(j) {
+            console.log(j);
         }).catch(function(error) {  
           console.log('Request failed', error)  
         });
