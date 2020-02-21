f0a7077818b33addcccc91a92dbc160d6575e9d2
AllenBattles <abattles@frameworkit.net>
2020-02-21 15:04:07 -0600

    Project Summary updates and binding

--- src/js/projectsummary.js
+++ src/js/projectsummary.js
@@ -23,11 +23,38 @@ t.render(function(){
         }).then(function(j) {
             
             var data = JSON.stringify(j);
-            document.getElementById('project_summary_content').innerHTML  = data;
-            document.getElementById('JTD_Total').innerHTML  = j.JTD_Total.toString();
-            document.getElementById('EAC_Total').innerHTML  = j.EAC_Total.toString();
-            document.getElementById('Base_Total').innerHTML  = j.Base_Total.toString();
-            document.getElementById('TotalComp').innerHTML  = j.TotalComp.toString();
+            if (j != null){
+                document.getElementById('project_summary_content').innerHTML  = data;
+
+                document.getElementById('TotalComp').innerHTML  = j.TotalComp.toString();
+
+                document.getElementById('JTD_Total').innerHTML  = j.JTD_Total.toString();                
+                document.getElementById('EAC_Total').innerHTML  = j.EAC_Total.toString();
+                document.getElementById('Base_Total').innerHTML  = j.Base_Total.toString();
+                document.getElementById('GrossVar').innerHTML  = j.GrossVar.toString();
+             
+                document.getElementById('JTD_Net').innerHTML  = j.JTD_Net.toString();
+                document.getElementById('EAC_Net').innerHTML  = j.EAC_Net.toString();
+                document.getElementById('Base_Net').innerHTML  = j.Base_Net.toString();
+                document.getElementById('NetVar').innerHTML  = j.NetVar.toString();
+                
+                document.getElementById('JTD_GM').innerHTML  = j.JTD_GM.toString();
+                document.getElementById('EAC_GM').innerHTML  = j.EAC_GM.toString();
+                document.getElementById('Base_GM').innerHTML  = j.Base_GM.toString();
+                document.getElementById('GMVar').innerHTML  = j.GMVar.toString();
+
+                document.getElementById('JTD_GMPct').innerHTML  = j.JTD_GMPct.toString();
+                document.getElementById('EAC_GMPct').innerHTML  = j.EAC_GMPct.toString();
+                document.getElementById('Base_GMPct').innerHTML  = j.Base_GMPct.toString();
+                document.getElementById('GMPctVar').innerHTML  = j.GMPctVar.toString();
+
+                document.getElementById('JTD_Mult').innerHTML  = j.JTD_Mult.toString();
+                document.getElementById('EAC_Mult').innerHTML  = j.EAC_Mult.toString();
+                document.getElementById('Base_Mult').innerHTML  = j.Base_Mult.toString();
+                document.getElementById('MultVar').innerHTML  = j.MultVar.toString();            
+
+            }
+
 
         }).catch(function(error) {  
           console.log('Request failed', error)  
