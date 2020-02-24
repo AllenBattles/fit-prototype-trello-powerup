77ad679b28d54b7be3c6492a4d1625547eeb5713
AllenBattles <abattles@frameworkit.net>
2020-02-24 17:31:19 -0600

    Project Summary settings

--- src/js/projectsummary.js
+++ src/js/projectsummary.js
@@ -62,81 +62,90 @@ t.render(function () {
     // recall this method as the user adds and removes attachments
     // from your section
 
-    t.card('all')
-        .then(function (card) {
-            //console.log(JSON.stringify(card, null, 2));
-            if (card) {
+    t.all([t.get('board', 'shared'), card('all')])
+    .then(([prefs, card]) => {
+        //get custom value
+        var customFieldID = "5e3c29b01d0b6b0e7371e4e3";
+        if (prefs && typeof prefs == 'object'){
+            customFieldID = prefs['projectsummary'];
+        }
 
-                var id = "NA";
-                var desc = card.desc;
-                if (desc && desc.length > 0)
-                    id = desc;
+        if (card) {
 
-                //get project number from custom field
-                if (card.customFieldItems && card.customFieldItems.length > 0) {
-                    for (var i = 0; i < card.customFieldItems.length; i++) {
-                        if (card.customFieldItems[i].idCustomField === "5e3c29b01d0b6b0e7371e4e3") {
-                            if (card.customFieldItems[i].value) {
-                                id = card.customFieldItems[i].value.text;
-                                break;
-                            }
+            var id = "NA";
+            var desc = card.desc;
+            if (desc && desc.length > 0)
+                id = desc;
+
+            //get project number from custom field
+            if (card.customFieldItems && card.customFieldItems.length > 0) {
+                for (var i = 0; i < card.customFieldItems.length; i++) {
+                    if (card.customFieldItems[i].idCustomField === "5e3c29b01d0b6b0e7371e4e3") {
+                        if (card.customFieldItems[i].value) {
+                            id = card.customFieldItems[i].value.text;
+                            break;
                         }
                     }
                 }
+            }
 
-                fetch(`${API_BASE}?id=${id}`)
-                    .then(function (response) {
-                        return response.json();
-                    }).then(function (j) {
-                        //var data = JSON.stringify(j);
-                        if (j != null) {
+            fetch(`${API_BASE}?id=${id}`)
+                .then(function (response) {
+                    return response.json();
+                }).then(function (j) {
+                    //var data = JSON.stringify(j);
+                    if (j != null) {
 
-                            try {
-                                document.getElementById('TotalComp').innerHTML = formatCurrency(j.TotalComp);
+                        try {
+                            document.getElementById('TotalComp').innerHTML = formatCurrency(j.TotalComp);
 
-                                document.getElementById('JTD_Total').innerHTML = formatCurrency(j.JTD_Total);
-                                document.getElementById('EAC_Total').innerHTML = formatCurrency(j.EAC_Total);
-                                document.getElementById('Base_Total').innerHTML = formatCurrency(j.Base_Total);
-                                document.getElementById('GrossVar').innerHTML = formatCurrency(j.GrossVar);
+                            document.getElementById('JTD_Total').innerHTML = formatCurrency(j.JTD_Total);
+                            document.getElementById('EAC_Total').innerHTML = formatCurrency(j.EAC_Total);
+                            document.getElementById('Base_Total').innerHTML = formatCurrency(j.Base_Total);
+                            document.getElementById('GrossVar').innerHTML = formatCurrency(j.GrossVar);
 
-                                document.getElementById('JTD_Net').innerHTML = formatCurrency(j.JTD_Net);
-                                document.getElementById('EAC_Net').innerHTML = formatCurrency(j.EAC_Net);
-                                document.getElementById('Base_Net').innerHTML = formatCurrency(j.Base_Net);
-                                document.getElementById('NetVar').innerHTML = formatCurrency(j.NetVar);
+                            document.getElementById('JTD_Net').innerHTML = formatCurrency(j.JTD_Net);
+                            document.getElementById('EAC_Net').innerHTML = formatCurrency(j.EAC_Net);
+                            document.getElementById('Base_Net').innerHTML = formatCurrency(j.Base_Net);
+                            document.getElementById('NetVar').innerHTML = formatCurrency(j.NetVar);
 
-                                document.getElementById('JTD_GM').innerHTML = formatCurrency(j.JTD_GM);
-                                document.getElementById('EAC_GM').innerHTML = formatCurrency(j.EAC_GM);
-                                document.getElementById('Base_GM').innerHTML = formatCurrency(j.Base_GM);
-                                document.getElementById('GMVar').innerHTML = formatCurrency(j.GMVar);
+                            document.getElementById('JTD_GM').innerHTML = formatCurrency(j.JTD_GM);
+                            document.getElementById('EAC_GM').innerHTML = formatCurrency(j.EAC_GM);
+                            document.getElementById('Base_GM').innerHTML = formatCurrency(j.Base_GM);
+                            document.getElementById('GMVar').innerHTML = formatCurrency(j.GMVar);
 
-                                document.getElementById('JTD_GMPct').innerHTML = formatPercent(j.JTD_GMPct, false);
-                                document.getElementById('EAC_GMPct').innerHTML = formatPercent(j.EAC_GMPct, false);
-                                document.getElementById('Base_GMPct').innerHTML = formatPercent(j.Base_GMPct, false);
-                                document.getElementById('GMPctVar').innerHTML = formatPercent(j.GMPctVar, false);
+                            document.getElementById('JTD_GMPct').innerHTML = formatPercent(j.JTD_GMPct, false);
+                            document.getElementById('EAC_GMPct').innerHTML = formatPercent(j.EAC_GMPct, false);
+                            document.getElementById('Base_GMPct').innerHTML = formatPercent(j.Base_GMPct, false);
+                            document.getElementById('GMPctVar').innerHTML = formatPercent(j.GMPctVar, false);
 
-                                document.getElementById('JTD_Mult').innerHTML = formatDecimal(j.JTD_Mult);
-                                document.getElementById('EAC_Mult').innerHTML = formatDecimal(j.EAC_Mult);
-                                document.getElementById('Base_Mult').innerHTML = formatDecimal(j.Base_Mult);
-                                document.getElementById('MultVar').innerHTML = formatDecimal(j.MultVar);
+                            document.getElementById('JTD_Mult').innerHTML = formatDecimal(j.JTD_Mult);
+                            document.getElementById('EAC_Mult').innerHTML = formatDecimal(j.EAC_Mult);
+                            document.getElementById('Base_Mult').innerHTML = formatDecimal(j.Base_Mult);
+                            document.getElementById('MultVar').innerHTML = formatDecimal(j.MultVar);
 
-                                document.getElementById('Client').innerHTML = "Client: " + j.Client;
-                                document.getElementById('ProjectManager').innerHTML = j.ProjectManager;
+                            document.getElementById('Client').innerHTML = "Client: " + j.Client;
+                            document.getElementById('ProjectManager').innerHTML = j.ProjectManager;
 
-                                document.getElementById('Owner').innerHTML = "Owner: " + j.Owner;
-                                document.getElementById('OwnerManager').innerHTML = j.OwnerManager;
-                            } catch (err) {
-                                console.log('Binding HTML Failed', err);
-                            }
-
+                            document.getElementById('Owner').innerHTML = "Owner: " + j.Owner;
+                            document.getElementById('OwnerManager').innerHTML = j.OwnerManager;
+                        } catch (err) {
+                            console.log('Binding HTML Failed', err);
                         }
 
+                    }
 
-                    }).catch(function (error) {
-                        console.log('Request failed', error)
-                    });
 
-            }
-        });
+                }).catch(function (error) {
+                    console.log('Request failed', error)
+                });
+
+        }
+
+    })
+
+
+ 
 
     //   t.getAll()
     //   .then(function(card){
--- src/js/setfield.js
+++ src/js/setfield.js
@@ -3,7 +3,7 @@ var t = TrelloPowerUp.iframe();
 window.setfield.addEventListener('submit', function (event) {
     // Stop the browser trying to submit the form itself.
     event.preventDefault();
-    return t.set('card', 'shared', 'projectsummary', window.customFieldSel.value)
+    return t.set('board', 'shared', 'projectsummary', window.customFieldSel.value)
         .then(function () {
             t.closePopup();
         });
@@ -31,7 +31,7 @@ t.render(function () {
             }
 
         }).then(function () {
-            t.sizeTo('#customFieldSel').done();
+            t.sizeTo(document.body).done();
         });
 
 });
